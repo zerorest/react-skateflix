a87f0264f59d6156a57798dadb027cee9c74dc0c
Jose Rodrigues <jrjoserodrigues42@gmail.com>
2020-11-20 16:45:30 -0300

    Setup inicial do projeto

--- src/App.js
+++ src/App.js
@@ -6,36 +6,36 @@ import Carousel from './components/Carousel';
 import Footer from './components/Footer';
 
 function App() {
-    return ( <
-        div style = {
-            { background: "#141414" } } >
+    return (<
+        div style={
+            { background: "#141414" }} >
         <
-        Menu / >
+            Menu />
 
         <
-        BannerMain videoTitle = { dadosIniciais.categorias[0].videos[0].titulo }
-        url = { dadosIniciais.categorias[0].videos[0].url }
-        videoDescription = { "O que é Front-end? Trabalhando na área, os termos HTML, CSS e Javascript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!" }
+            BannerMain videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+            url={dadosIniciais.categorias[0].videos[0].url}
+            videoDescription={"What is a flip? Working on a trick, the terms, technics, vibes, kind and contemplantion that make part of the daily routine of the skater. But what do they do, after all? Check it out!"}
         />
 
         <
-        Carousel ignoreFirstVideo category = { dadosIniciais.categorias[0] }
+            Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]}
         />
 
         <
-        Carousel category = { dadosIniciais.categorias[1] }
+            Carousel category={dadosIniciais.categorias[1]}
         />
 
         <
-        Carousel category = { dadosIniciais.categorias[2] }
+            Carousel category={dadosIniciais.categorias[2]}
         />
 
         <
-        Carousel category = { dadosIniciais.categorias[3] }
+            Carousel category={dadosIniciais.categorias[3]}
         />
 
         <
-        Footer
+            Footer
 
         /
         >
